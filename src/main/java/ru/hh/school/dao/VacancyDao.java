48a8765692ee7ea3e8dbb7f96f426a395583c29f
Anton Shtyk <th@tshtk.ru>
2022-02-16 10:37:47 +0300

    fix: change query StatisticsDto

--- src/main/java/ru/hh/school/dao/VacancyDao.java
+++ src/main/java/ru/hh/school/dao/VacancyDao.java
@@ -10,10 +10,8 @@ public class VacancyDao extends GenericDao{
   }
 
   public StatisticsDto getSalaryStatistics(Area area){
-    // ToDo дополните запрос, чтобы возвращался ru.hh.school.employers.StatisticsDto
-    // https://vladmihalcea.com/the-best-way-to-map-a-projection-query-to-a-dto-with-jpa-and-hibernate/
     return getSession().createQuery(
-        "SELECT count(v.id), min(v.compensationFrom), max(v.compensationTo) " +
+        "SELECT new ru.hh.school.employers.StatisticsDto (count(v.id), min(v.compensationFrom), max(v.compensationTo)) " +
             "FROM Vacancy v WHERE v.area = :area", StatisticsDto.class)
         .setParameter("area", area)
         .getSingleResult();
