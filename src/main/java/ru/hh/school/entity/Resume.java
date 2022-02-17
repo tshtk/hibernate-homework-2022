292453e1914f28c523182a869c47728a3bede763
Anton Shtyk <th@tshtk.ru>
2022-02-17 09:00:48 +0300

    fix: change generation type for id to sequence in Resume

--- src/main/java/ru/hh/school/entity/Resume.java
+++ src/main/java/ru/hh/school/entity/Resume.java
@@ -1,29 +1,23 @@
 package ru.hh.school.entity;
 
-import javax.persistence.GeneratedValue;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.SequenceGenerator;
 
-//TODO: оформите entity
+
+@Entity
 public class Resume {
-  // TODO: сделать так, чтобы id брался из sequence-а
-  // таким образом, мы сможем отправлять в бд запросы батчами.
-  // нужно учитывать, что описание sequence при создании таблицы также должно соответствовать
-  // хиберовской сущности (см. create_resume.sql)
-  //
-  // Подробнее:
-  // https://vladmihalcea.com/how-to-batch-insert-and-update-statements-with-hibernate/
-  // https://vladmihalcea.com/from-jpa-to-hibernates-legacy-and-enhanced-identifier-generators/
-
   @Id
-  @GeneratedValue(/* здесь место для вашего кода */)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="resume_id")
+  @SequenceGenerator(name="resume_id", sequenceName="resume_id_seq", allocationSize=10)
   private Integer id;
-
   private String description;
 
-  Resume() {}
+  public Resume() {}
 
   public Resume(String description) {
     this.description = description;
   }
-
 }
